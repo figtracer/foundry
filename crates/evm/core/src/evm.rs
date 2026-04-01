abee0d907e1c72147ead9ce30ef3c43d08a0a950
figtracer <1gusredo@gmail.com>
2026-04-01 18:23:49 +0100

    chore(evm): remove redundant `Clone` bound implied by `Copy`

--- crates/evm/core/src/evm.rs
+++ crates/evm/core/src/evm.rs
@@ -45,7 +45,7 @@ use tempo_revm::{
 /// layers without depending on a concrete EVM type.
 pub trait FoundryEvmFactory:
     EvmFactory<
-        Spec: Into<SpecId> + Default + Copy + Clone + Unpin + Send + 'static,
+        Spec: Into<SpecId> + Default + Copy + Unpin + Send + 'static,
         BlockEnv: FoundryBlock + ForkBlockEnv + Default + Unpin,
         Precompiles = PrecompilesMap,
     > + Clone
@@ -56,7 +56,7 @@ pub trait FoundryEvmFactory:
 
 impl<
     F: EvmFactory<
-            Spec: Into<SpecId> + Default + Copy + Clone + Unpin + Send + 'static,
+            Spec: Into<SpecId> + Default + Copy + Unpin + Send + 'static,
             BlockEnv: FoundryBlock + ForkBlockEnv + Default + Unpin,
             Precompiles = PrecompilesMap,
         > + Clone
